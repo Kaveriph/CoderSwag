a861e504699e5561a5cd5fee2cac2180f09f0f21
Kaveri <kaveriphi915@gmail.com>
2017-11-12 20:55:07 +0530

    icluded the resources and object singleton

--- app/src/main/java/com/example/kaveri/codeerswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/kaveri/codeerswag/controller/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.kaveri.codeerswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.kaveri.codeerswag.R
+import com.example.kaveri.codeerswag.model.Category
+import com.example.kaveri.codeerswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialise()
+    }
+
+    fun initialise() {
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoriesList.adapter = adapter
     }
 }
--- app/src/main/java/com/example/kaveri/codeerswag/model/Category.kt
+++ app/src/main/java/com/example/kaveri/codeerswag/model/Category.kt
@@ -3,4 +3,8 @@ package com.example.kaveri.codeerswag.model
 /**
  * Created by KAVERI on 11/12/2017.
  */
-class Category(val title:String, val image:String )
+class Category(val title:String, val image:String ) {
+    override fun toString(): String {
+        return title
+    }
+}
